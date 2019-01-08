d61abe5cac9d83008337686adb715e0878e65d39
Mark <usertzc@gmail.com>
2019-01-08 13:47:43 +0800

    Update loadinstall.sh

--- docker-alpine-Redis/5.0/loadinstall.sh
+++ docker-alpine-Redis/5.0/loadinstall.sh
@@ -1,3 +1,3 @@
 
-curl -Lk https://raw.githubusercontent.com/marksugar/dockerMops/master/docker-alpine-Redis/5.0/docker-compose.yaml -o $PWD/docker-compose.yaml
-docker-compose up -d
+curl -Lk https://raw.githubusercontent.com/marksugar/dockerMops/master/docker-alpine-Redis/5.0/docker-compose.yaml -o $PWD/docker-compose-redis.yaml
+docker-compose -f $PWD/docker-compose-redis.yaml up -d
