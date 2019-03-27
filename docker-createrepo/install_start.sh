af478d93d76a23d7d3af08e0a6951db7812f509c
Mark <usertzc@gmail.com>
2019-03-27 21:52:44 +0800

    Update install_start.sh

--- docker-createrepo/install_start.sh
+++ docker-createrepo/install_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DPATH=/data/mirrors
 mkdir -p ${DPATH}
-curl -Lk https://raw.githubusercontent.com/marksugar/dockerMops/master/docker-createrepo/docker-compose.yml -o ${DPATH}/docker-compose.yml 
+curl -Lk https://raw.githubusercontent.com/marksugar/dockerMops/master/docker-createrepo/docker-compose.yml -o ${DPATH}/docker-compose.yml
 docker-compose -f ${DPATH}/docker-compose.yml up -d
