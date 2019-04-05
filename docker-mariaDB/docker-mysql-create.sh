bfce000fc8ce96ab936c956fd36f20829a6cad20
Mark <usertzc@gmail.com>
2019-04-05 19:41:23 +0800

    Update docker-mysql-create.sh

--- docker-mariaDB/docker-mysql-create.sh
+++ docker-mariaDB/docker-mysql-create.sh
@@ -10,8 +10,8 @@ jpdir=/data/docker/mysql/
 mkdir $jpdir/data /data/logs/mysql -p
 #curl -Lks4 https://raw.githubusercontent.com/LinuxEA-Mark/Docker-MariaDB/master/initialization.sh -o ${jpdir}initialization.sh
 #curl -Lks4 https://raw.githubusercontent.com/LinuxEA-Mark/Docker-MariaDB/master/initialization.sql -o ${jpdir}initialization.sql
-curl -Lks4 https://raw.githubusercontent.com/LinuxEA-Mark/Docker-MariaDB/master/my.cnf -o ${jpdir}my.cnf
+curl -Lks4 https://raw.githubusercontent.com/marksugar/dockerMops/master/docker-mariaDB/my.cnf -o ${jpdir}my.cnf
 #/usr/bin/docker
-curl -Lks4 https://raw.githubusercontent.com/LinuxEA-Mark/Docker-MariaDB/master/docker-compose.yaml -o ${jpdir}docker-compose.yaml
+curl -Lks4 https://raw.githubusercontent.com/marksugar/dockerMops/master/docker-mariaDB/docker-compose.yaml -o ${jpdir}docker-compose.yaml
 #/usr/local/bin/docker-compose -f /jumpserver/docker-compose.yaml up -d
 cd $jpdir && /usr/local/bin/docker-compose up -d
