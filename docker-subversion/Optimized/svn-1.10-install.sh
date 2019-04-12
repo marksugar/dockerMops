1c68e702a9be6c45ef8eb39a0e73461f63449b7b
Mark <usertzc@gmail.com>
2019-04-12 18:31:24 +0800

    Update svn-1.10-install.sh

--- docker-subversion/Optimized/svn-1.10-install.sh
+++ docker-subversion/Optimized/svn-1.10-install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir /data/docker/ -p
-curl -Lks4 https://raw.githubusercontent.com/LinuxEA-Mark/docker-subversion1.9.4/master/Optimized/docker-compose.yaml -o /data/docker/docker-compose.yaml
+curl -Lks4 https://raw.githubusercontent.com/marksugar/dockerMops/master/docker-subversion/Optimized/docker-compose.yaml -o /data/docker/docker-compose.yaml
 /usr/local/bin/docker-compose -f /data/docker/docker-compose.yaml up -d
