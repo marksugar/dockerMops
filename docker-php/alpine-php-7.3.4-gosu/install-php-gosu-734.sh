d05e603d92f58129547cd9e32faace39098fddc1
mark <usertzc@gmail.com>
2019-04-17 21:56:20 +0800

    修改脚本

--- docker-php/alpine-php-7.3.4-gosu/install-php-gosu-734.sh
+++ docker-php/alpine-php-7.3.4-gosu/install-php-gosu-734.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-PATH=/usr/local/php/etc
-mkdir $PATH -p
-curl -Lk  https://raw.githubusercontent.com/marksugar/dockerMops/master/docker-php/alpine-php-7.3.4-gosu/php-fpm.conf -o $PATH/php-fpm.conf
-curl -Lk  https://raw.githubusercontent.com/marksugar/dockerMops/master/docker-php/alpine-php-5.6.40/php.ini -o $PATH/php.ini
+PHPATH=/usr/local/php/etc
+mkdir $PHPATH -p
+curl -Lk  https://raw.githubusercontent.com/marksugar/dockerMops/master/docker-php/alpine-php-7.3.4-gosu/php-fpm.conf -o $PHPATH/php-fpm.conf
+curl -Lk  https://raw.githubusercontent.com/marksugar/dockerMops/master/docker-php/alpine-php-5.6.40/php.ini -o $PHPATH/php.ini
 curl -Lk  https://raw.githubusercontent.com/marksugar/dockerMops/master/docker-php/alpine-php-7.3.4-gosu/docker-compose.yml -o $PWD/docker-compose.yml
 docker-compose $PWD/docker-compose.yml up -d
